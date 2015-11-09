a62cca7ce0dba09c3caa23f1075aea07079dc2e2
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2015-11-09 14:20:43 -0500

    class methods example

--- rectangle.rb
+++ rectangle.rb
@@ -7,6 +7,18 @@ class Rectangle
     @height = height
   end
 
+  def self.rectangles(rectangle_data)
+    # rectangles = []
+    # rectangle_data.each do |rectangle_datum|
+      # rectangles << Rectangle.new(rectangle_datum[0], rectangle_datum[1])
+    # end
+    # rectangles
+
+    rectangle_data.map do |rectangle_datum|
+      Rectangle.new(rectangle_datum[0], rectangle_datum[1])
+    end
+  end
+
   def area
     width * height
   end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -4,6 +4,15 @@ require_relative '../rectangle'
 describe Rectangle do
   let(:rectangle) { Rectangle.new(10, 5) }
 
+  describe ".rectangles" do
+    it "should create new rectangle objects" do
+      rectangle_data = [[10, 5], [20, 10], [1, 3]]
+      Rectangle.rectangles(rectangle_data).each do |rectangle|
+        expect(rectangle).to be_a(Rectangle)
+      end
+    end
+  end
+
   describe ".new" do
     it "should create a new Rectangle" do
       expect(rectangle).to be_a(Rectangle)
